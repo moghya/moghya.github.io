b0d99dbeb104d234e733a701b6cc39cbed7110dd
Shubham Sawant <shubham.sawant@walchandsangli.ac.in>
2017-08-05 12:55:05 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -141,7 +141,9 @@ function onBodyLoad(){
 }
 
 $(document).ready(function(){
-
+	$('.collapsible').collapsible({
+	  'accordion' : true
+	});
 	$('#tabs').tabs({ 'swipeable': true });
 	$('.info').perfectScrollbar();
 	onWindowResize();
